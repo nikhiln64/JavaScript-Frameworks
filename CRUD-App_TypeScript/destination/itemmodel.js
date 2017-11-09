858c330f97ebceb8e4bdf494ea5bffdc15593cbb
Nikhil Bharadwaj <nikhiln64@gmail.com>
2017-11-08 23:48:08 -0500

    JS
    JavaScript files commit

---
+++ CRUD-App_TypeScript/destination/itemmodel.js
@@ -0,0 +1,11 @@
+class Item {
+    constructor(id, name, desc, price, image, color) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.image = image;
+        this.color = color;
+        this.markForDelete = false;
+    }
+}
---
+++ CRUD-App_TypeScript/destination/utils.js
@@ -0,0 +1,10 @@
+function* autoGenNumber() {
+    var num = 1000;
+    while (true) {
+        num++;
+        var reset = yield num;
+        if (reset) {
+            num = 1000;
+        }
+    }
+}
